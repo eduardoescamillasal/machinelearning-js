ba0ca57cc196472b9056fc9f482e5a8b5ad5b295
Eduardo Escamilla Saldaña <eduardo.escamillasal@gmail.com>
2025-02-27 12:08:46 -0600

    knn on tf

--- knn-tf/index.js
+++ knn-tf/index.js
@@ -2,6 +2,22 @@ require("@tensorflow/tfjs-node");
 const tf = require("@tensorflow/tfjs");
 const loadCSV = require("./load-csv");
 
+function knn(features, labels, predictionPoint, k) {
+  return (
+    features
+      .sub(predictionPoint)
+      .pow(2)
+      .sum(1)
+      .pow(0.5)
+      .expandDims(1)
+      .concat(labels, 1)
+      .unstack()
+      .sort((a, b) => a.get(0) - b.get(0))
+      .slice(0, k)
+      .reduce((acc, pair) => acc + pair.get(1), 0) / k
+  );
+}
+
 let { features, labels, testFeatures, testLabels } = loadCSV(
   "kc_house_data.csv",
   {
@@ -12,20 +28,4 @@ let { features, labels, testFeatures, testLabels } = loadCSV(
   }
 );
 
-console.log(testFeatures);
-console.log(testLabels);
-
-// const predictionPoint = tf.tensor([-121, 47]);
-// const k = 2;
-
-// features
-//   .sub(predictionPoint)
-//   .pow(2)
-//   .sum(1)
-//   .pow(0.5)
-//   .expandDims(1)
-//   .concat(labels, 1)
-//   .unstack()
-//   .sort((a, b) => a.get(0) - b.get(0))
-//   .slice(0, k)
-//   .reduce((acc, pair) => acc + pair.get(1), 0) / k;
+const result = knn();
